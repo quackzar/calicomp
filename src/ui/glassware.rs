3ca9c3d24440b0928646b7eeaea9b4d35f7e1693
Mikkel Wienberg Madsen <mikkel.wienberg@gmail.com>
2024-09-10 23:18:27 +0200

    martini

--- src/ui/glassware.rs
+++ src/ui/glassware.rs
@@ -1,12 +1,81 @@
 use ratatui::{
     prelude::*,
     widgets::{
-        canvas::{self, Line, Shape},
+        canvas::{self, Context, Line, Shape},
         Block, Widget,
     },
 };
 
-pub struct Martini {}
+pub struct Martini {
+    foot_width: f64,
+    head_width: f64,
+    head_height: f64,
+    stilk_height: f64,
+    foot_height: f64,
+    stilk_width: f64,
+}
+
+impl Martini {
+    pub fn draw(&self, ctx: &mut Context<'_>, color: Color) {
+            ctx.draw(&Line {
+                x1: -self.stilk_width/2.0,
+                y1: 0.0,
+                x2: -self.stilk_width/2.0,
+                y2: self.stilk_height,
+                color,
+            });
+            ctx.draw(&Line {
+                x1: self.stilk_width/2.0,
+                y1: 0.0,
+                x2: self.stilk_width/2.0,
+                y2: self.stilk_height,
+                color,
+            });
+            ctx.draw(&Line {
+                x1: self.stilk_width/2.0,
+                y1: self.stilk_height,
+                x2: self.head_width/2.0,
+                y2: self.head_height - self.stilk_height,
+                color,
+            });
+            ctx.draw(&Line {
+                x1: -self.stilk_width/2.0,
+                y1: self.stilk_height,
+                x2: -self.head_width/2.0,
+                y2: self.head_height - self.stilk_height,
+                color,
+            });
+            ctx.draw(&Line {
+                x1: -self.stilk_width/2.0,
+                y1: self.head_height - self.stilk_height,
+                x2: self.stilk_width/2.0,
+                y2: self.head_height - self.stilk_height,
+                color,
+            });
+            ctx.draw(&Line {
+                x1: -self.stilk_width,
+                y1: 0.0,
+                x2: -self.foot_width,
+                y2: -self.foot_height,
+                color,
+            });
+            ctx.draw(&Line {
+                x1: self.stilk_width,
+                y1: 0.0,
+                x2: self.foot_width,
+                y2: -self.foot_height,
+                color,
+            });
+            ctx.draw(&Line {
+                x1: -self.foot_width,
+                y1: self.foot_height,
+                x2: self.foot_width,
+                y2: -self.foot_height,
+                color,
+            });
+
+    }
+}
 
 pub fn draw_cocktail(area: Rect, buf: &mut Buffer) {
     canvas::Canvas::default()
